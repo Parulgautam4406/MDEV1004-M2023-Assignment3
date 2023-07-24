ebec541a150b704dee825acabb1bb757ec5151ff
Parul Gautam <200538922@student.georgianc.on.ca>
2023-07-23 21:24:34 -0400

    Added Movie Table View Cell

---
+++ MDEV1004-M2023-Assignment3/MDEV1004-M2023-Assignment3/MovieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.swift
+//  MDEV1004-M2023-Assignment3
+//
+//  Created by Parul Gautam on 23/07/23.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UITextField!
+    
+    @IBOutlet weak var studioLabel: UITextField!
+    
+    @IBOutlet weak var ratingLabel: UITextField!
+}
